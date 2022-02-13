5a66387a6695ff7323c799a48a85d2f6120ed347
Jeffrey Miller <Mill7707@umn.edu>
2022-02-13 16:23:50 -0600

    Added Fish objects

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private MeshRenderer meshRenderer;
+    private Color c;
+    private float lastHitTime = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((Time.time - lastHitTime) > 0.25f)
+        {
+            lastHitTime = Time.time;
+            audioSource.volume = Mathf.InverseLerp(0, 10, collision.relativeVelocity.magnitude);
+            audioSource.pitch = Random.Range(0.75f, 1.25f);
+            audioSource.Play();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
